22bc7bd0e6dd9f0ad27f3a47642544b2c45c94a6
ThePyLord <geraldfeu@gmail.com>
2020-12-28 00:05:28 -0600

    Added the first real part of the scraping section

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ from bs4 import BeautifulSoup
 URL = "https://www.google.com/search?q=btc+usd&oq=BTC+usd&aqs=chrome.0.69i59j0i67i131i433j0i131i433j0l2j69i60l3.2445j0j7&sourceid=chrome&ie=UTF-8"
 site = requests.get(URL)
 
-soup= BeautifulSoup(site.content, 'html.parser')
+soup = BeautifulSoup(site.content, 'html.parser')
 
+with open("priceinfo.txt", "w") as f:
+    f.write(soup.text)
+    f.close()
