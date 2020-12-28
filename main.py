2e6f3027784ac6b01eeec630ce77fa599d5c9532
ThePyLord <geraldfeu@gmail.com>
2020-12-27 22:52:14 -0600

    import BeautifulSoup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #main file for the project
 
 import requests
+from bs4 import BeautifulSoup
+
 
 URL = "https://www.google.com/search?q=btc+usd&oq=BTC+usd&aqs=chrome.0.69i59j0i67i131i433j0i131i433j0l2j69i60l3.2445j0j7&sourceid=chrome&ie=UTF-8"
