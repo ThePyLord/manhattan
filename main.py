47832cc707cbe1e00efe755a585c626b49b9fd64
ThePyLord <geraldfeu@gmail.com>
2020-12-27 22:49:06 -0600

    added the URL

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 #main file for the project
 
-import os
+import requests
+
+URL = "https://www.google.com/search?q=btc+usd&oq=BTC+usd&aqs=chrome.0.69i59j0i67i131i433j0i131i433j0l2j69i60l3.2445j0j7&sourceid=chrome&ie=UTF-8"
