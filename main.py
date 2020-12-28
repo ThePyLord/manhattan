85343e0f7b83bf9b7af8b42541e734df76b3dcfd
ThePyLord <geraldfeu@gmail.com>
2020-12-27 23:57:34 -0600

    added the request to the website and some other modules

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #main file for the project
 
-import requests
+import requests, discord
+import json
 from bs4 import BeautifulSoup
 
 
 URL = "https://www.google.com/search?q=btc+usd&oq=BTC+usd&aqs=chrome.0.69i59j0i67i131i433j0i131i433j0l2j69i60l3.2445j0j7&sourceid=chrome&ie=UTF-8"
+site = requests.get(URL)
+
+soup= BeautifulSoup(site.content, 'html.parser')
+
