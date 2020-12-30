13844ec56e5815246adc7099a43c9d6aacdb9e8e
ThePyLord <geraldfeu@gmail.com>
2020-12-30 00:46:54 -0600

    deleted some whitespace

--- discord_agent.py
+++ discord_agent.py
@@ -1,7 +1,6 @@
 WEBHOOK = "https://discord.com/api/webhooks/793726582060875786/elSlN6AF_qZNyR4rsfsooYjIjbZi7GCvpmHnBFB7y-QqivMxM5WCU6sBcc0kDHf91NSl" #insert your own channel's webhook
 import discord
 
-
 client = discord.Client()
 
 @client.event
