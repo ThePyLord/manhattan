7abcd46676205ebe511317a755291037012931a0
ThePyLord <geraldfeu@gmail.com>
2021-01-05 11:03:29 -0600

    uploading the most recent changes

--- discord_agent.py
+++ discord_agent.py
@@ -14,24 +14,38 @@ client = discord.Client()
 """
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+	for guild in client.guilds:
+		if guild.name == GUILD:
+			break
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+	print(
+		f'{client.user} is connected to the following guild:\n'
+		f'{guild.name}(id: {guild.id})'
+	)
  """
 
 @client.event
 async def on_ready():
-    print(f"Hot n' ready, My name is {client.user}")
+	print(f"Hot n' ready, My name is {client.user}")
 
+#Send a message to the intended recipient and guild 
 @client.event
 async def on_message(message):
-    if "!btc price usd" in message.content.lower():
-        await message.channel.send(main.btc_value_text)
-        await client.close()        
+	id = client.get_guild(GUILD_ID)
+	if "!btc price usd" in message.content.lower():
+		await message.channel.send(main.btc_value_text)
+		# await client.close()       
+
+	elif "!users" in message.content.lower():
+		await message.channel.send(f"There's {id.member_count} users in this server")
+
+	elif "!bye" in message.content.lower():
+		await client.close()
+
+	elif "!clear" in message.content.lower():
+		pass
+
+
+
 
 client.run(TOKEN)
