50777e69129d3edc970f045d4f5d612a03b630ab
Muhammad Ibrahim <i.muhammad000056@gmail.com>
2021-01-15 23:16:54 -0600

    Fixed up main

---
+++ cogs/Manhattan.py
@@ -0,0 +1,70 @@
+import os, sys
+
+import discord
+from discord.ext import commands
+#using relative import to import a module from a parent directory
+from tesco import search
+from portfolio import Portfolio
+
+current_dir = os.path.dirname(os.path.realpath((__file__)))
+root_dir = os.path.dirname(current_dir)
+sys.path.append(root_dir)
+
+class Manhattan(commands.Cog):
+	"""
+	Manhattan bot
+	-------------
+
+	The main object for all subcommands of the Manhattan bot a.k.a "Bitcoin's Eye"
+	....
+
+	Attributes:
+	-----------
+	manhattan
+		Used as "$manhattan".
+		Does not provide a response to the user
+
+	price
+		Used as "$manhattan price `<ticker>`". 
+		Responds with the price of the asset given
+
+	pft: Portfolio
+		Used as "$manhattan pft <ticker> <entry> <target(optional)>".
+		Creates a portfolio for the user
+	"""
+	def __init__(self, client):
+		self.client = client
+
+	@commands.group(name='manhattan', invoke_without_command=False)
+	async def manhattan(self, ctx):
+		await ctx.channel.send("Base group for the Manhattan bot")
+
+	@manhattan.command(name='price')
+	async def price(self, ctx, arg):
+		await ctx.channel.send(search.btc_value_text)
+
+	@manhattan.command(name='pft')
+	async def pft(self, ctx, ticker: str, entry: int, *, arg):
+		"""
+		Shows the portfolio only
+
+		Parameters
+		----------
+		ticker: str
+			The ticker symbol
+		
+		entry: int
+			The entry price of the asset specified
+		"""
+		self.pft = Portfolio()
+		self.pft.update_portfolio(ticker, entry, target=arg)
+		print(self.pft.portfolio)
+		await ctx.send(self.pft.portfolio)
+
+
+
+
+
+
+def setup(client):
+	client.add_cog(Manhattan(client))
---
+++ tesco/search.py
@@ -0,0 +1,11 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+URL = "https://www.google.com/search?q=btc+usd&oq=BTC+usd&aqs=chrome.0.69i59j0i67i131i433j0i131i433j0l2j69i60l3.2445j0j7&sourceid=chrome&ie=UTF-8"
+site = requests.get(URL)
+print(type(site), site, sep="\n")
+
+soup = BeautifulSoup(site.content, 'html.parser')
+btc_value = soup.find(class_="BNeawe iBp4i AP7Wnd")
+btc_value_text = btc_value.text + 's'
